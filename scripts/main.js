c1d7595d7312915bdea3cd376c81c77d5ad97c85
Alex Alexeev <alex.alexeev@kiwi.com>
2018-11-06 20:02:32 +0100

    Fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
-
 function changeText(x) {
   x.innerText = 'Привет мир!';
 }
-
 
 var headings = document.querySelectorAll('h1');
 headings.forEach(changeText);
@@ -10,8 +8,6 @@ headings.forEach(changeText);
 function sum(x, y) {
   return x + y;
 }
-
-const sum = (x,y) => {x + y}
 
 var list = [5, 4, 3, 6];
 
@@ -23,26 +19,30 @@ const text = 'lol1';
 
 let changeable = 1;
 
-const sfdsf = () => {
-  
-}
-
-con sdfsdf = {name : "Vasia", surname: "Lol"}
+const sdfsdf = { name: 'Vasia', surname: 'Lol' };
 
 const ssdf = [];
 
 const isBlue = true;
 
-let color = "green";
+let color = 'green';
 
 // тут я описал пример использования условного оператора
-// sdfdsfds  
-if (isBlue === true) { // fdsfdsfsdfsfsdf
-  color = "blue"
-} else if (isBlue === "sfsdf") {
-  color = "purple"
-} else if (sdfdsfds === "sfsdf") {
-  color = "purple"
+// sdfdsfds
+if (isBlue === true) {
+  // fdsfdsfsdfsfsdf
+  color = 'blue';
+} else if (isBlue === 'sfsdf') {
+  color = 'purple';
+} else if (sdfdsfds === 'sfsdf') {
+  color = 'purple';
 } else {
-  
 }
+
+const myImage = document.querySelector('img');
+
+myImage.onClick = () => {
+  console.log('Privet');
+};
+
+console.log('lol');
