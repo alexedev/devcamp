55c7012b399d418de6047bed6c634a55c4c61c30
Alex Alexeev <alex.alexeev@kiwi.com>
2018-11-06 18:58:11 +0100

    Some changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+
 function changeText(x) {
   x.innerText = 'Привет мир!';
 }
+
 
 var headings = document.querySelectorAll('h1');
 headings.forEach(changeText);
@@ -9,6 +11,38 @@ function sum(x, y) {
   return x + y;
 }
 
+const sum = (x,y) => {x + y}
+
 var list = [5, 4, 3, 5];
 
 sum(5, 4);
+
+var myVariable;
+
+const text = 'lol';
+
+let changeable = 1;
+
+const sfdsf = () => {
+
+}
+
+con sdfsdf = {name : "Vasia", surname: "Lol"}
+
+const ssdf = [];
+
+const isBlue = false;
+
+let color = "green";
+
+// тут я описал пример использования условного оператора
+// sdfdsfds  
+if (isBlue === true) { // fdsfdsfsdfsfsdf
+  color = "blue"
+} else if (isBlue === "sfsdf") {
+  color = "purple"
+} else if (sdfdsfds === "sfsdf") {
+  color = "purple"
+} else {
+  
+}
